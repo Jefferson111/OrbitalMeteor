fe555abe29109a981d42abd45a00a739a9b80a89
jun-ha0 <e0309667@u.nus.edu>
2019-06-28 11:01:20 +0800

    incomplete randomiser algo

--- client/js/home.js
+++ client/js/home.js
@@ -1,2 +1,38 @@
-Template.home.rendered = function () {
+Template.home.rendered = () => {
+}
+
+Template.home.events({
+    "click #cmdgen": () => {
+        console.log("cmdgen clicked");
+        const namesTextArea = document.querySelector("#names");
+        const namesData = namesTextArea.value;
+        const themesTextArea = document.querySelector("#themes");
+        const themesData = themesTextArea.value;
+        const actualGroupNamesTextArea = document.querySelector("#actualgroups");
+        const actualGroupNamesData = actualGroupNamesTextArea.value;
+        if (namesData !== "") {
+            const node = document.querySelector("#output .desc #groupings");
+            if (node.hasChildNodes()) {
+                node.innerHTML = ""; // clears innerHTML before renderData
+            }
+            renderData(namesData, themesData, actualGroupNamesData);
+        }
+    }
+});
+
+function renderData(namesData, themesData, actualGroupNamesData) {
+    const names = namesData.split(/[\s,]+/);
+    const groups = actualGroupNamesData.split(/[\s,]+/);
+    for (let i = 0; i < names.length; i++) {
+        makeRawData("Group" + ((themesData === "default" || groups[i] === "" ||
+            groups.length !== names.length) ? i + 1 : groups[i]) + ": " + names[i]);
+    }
+}
+
+function makeRawData(ele) {
+    const div = document.createElement("div");
+    const content = document.createElement("div");
+    content.innerHTML = ele;
+    div.appendChild(content);
+    document.querySelector("#output .desc #groupings").appendChild(div);
 }
